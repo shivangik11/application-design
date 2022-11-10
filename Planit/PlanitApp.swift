8e19bed2e0259b37455baaaa439b587e391462ab
shivangik11 <89596201+shivangik11@users.noreply.github.com>
2022-11-09 17:30:21 -0700

    push code to develop branch

---
+++ Planit/PlanitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanitApp.swift
+//  Planit
+//
+//  Created by Shivangi on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PlanitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Planit/View/Authentication/SignupView.swift
@@ -0,0 +1,25 @@
+//
+//  SignupView.swift
+//  Planit
+//
+//  Created by Shivangi on 09/11/22.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        
+        ZStack{
+            BackgroundGradientView()
+        }
+        
+        
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
