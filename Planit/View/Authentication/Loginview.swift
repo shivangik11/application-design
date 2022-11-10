c7043650c41e4a6d2c44dfcd2d17ac2d1417c128
shivangik11 <89596201+shivangik11@users.noreply.github.com>
2022-11-10 14:33:29 -0700

    logo added to login screen

--- Planit/View/Authentication/Loginview.swift
+++ Planit/View/Authentication/Loginview.swift
@@ -12,6 +12,9 @@ struct Loginview: View {
     
         ZStack{
             BackgroundGradientView()
+            VStack{
+                LogoView()
+            }
             
             
             
