59b922a65e78764c023a2d43d979c06fa39f7bee
shivangik11 <89596201+shivangik11@users.noreply.github.com>
2022-12-06 16:26:00 -0700

    added buttons

---
+++ Planit/View/Utils/PlanButtonStack.swift
@@ -0,0 +1,20 @@
+//
+//  PlanButtonStack.swift
+//  Planit
+//
+//  Created by Shivangi on 06/12/22.
+//
+
+import SwiftUI
+
+struct PlanButtonStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanButtonStack_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanButtonStack()
+    }
+}
